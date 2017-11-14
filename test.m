54bce64ebcc051384420757f94a2d4d73a76779b
Gabriel Rodrigues Rocha <gabrielrodrigues_rocha@hotmail.com>
2017-11-14 11:17:11 -0200

    Fix magic numbers

--- test.m
+++ test.m
@@ -1,7 +1,7 @@
 clc, clear, close all;
 
-image = imread('scenarios\wheresWaldo1.jpg');
-ref = imread('references\waldo2.jpg');
+image = imread('scenarios/wheresWaldo2.jpg');
+ref = imread('references/waldo2.jpg');
 [ih, iw, ~] = size(image);
 
 imageHsv = rgb2hsv(image);
@@ -22,19 +22,19 @@ whiteDilated = imdilate(whiteStripes, se);
 
 % Get their overlapped area
 roi = redDilated & whiteDilated;
-figure,imshow(roi), title("Segmentação por tiras vermelhas e brancas.");
+figure,imshow(roi), title('SegmentaÃ§Ã£o por tiras vermelhas e brancas.');
 
 % Remove smaller or bigger components that are not Wally
-roi = bwareaopen(roi, floor(ih/150 * iw/150));
-bigger = bwareaopen(roi, floor(ih/100 * iw/100));
+roi = bwareaopen(roi, floor(ih/85 * iw/85));
+bigger = bwareaopen(roi, floor(ih/50 * iw/50));
 roi = roi - bigger;
-figure,imshow(roi), title("Remoção de elementos menores que o Wally.");
+figure,imshow(roi), title('RemoÃ§Ã£o de elementos menores que o Wally.');
 
 % Purify Wally
 roi = roi & (redStripes | whiteStripes);
 roi = imdilate(roi, se);
 roi = bwareaopen(roi, 10);
-figure,imshow(roi), title("Dilatação e abertura de elementos restantes.");
+figure,imshow(roi), title('DilataÃ§Ã£o e abertura de elementos restantes.');
 
 % Find Wally throught normalized cross correlation
 roi = logical(roi);
@@ -45,8 +45,8 @@ hold on
 plot(centroids(:,1),centroids(:,2),'b*');
 hold off
 
-locus_x = round(centroids(6,2));
-locus_y = round(centroids(6,1));
+locus_x = round(centroids(10,2));
+locus_y = round(centroids(10,1));
 
 slice = image(locus_x-100:locus_x+100, locus_y-100:locus_y+100);
 ref = rgb2gray(ref);
