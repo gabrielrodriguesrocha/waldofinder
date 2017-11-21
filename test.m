32f603e64bfb9dde1b29a4d2fd8d0a230f62ffe3
gabrielrodriguesrocha <gabrielrodrigues_rocha@hotmail.com>
2017-11-21 11:08:34 -0200

    #SAVED

--- test.m
+++ test.m
@@ -1,8 +1,9 @@
 clc, clear, close all;
 
-image = imread('scenarios\wheresWaldo1.jpg');
+image = imread('scenarios\wheresWaldo4.jpg');
 ref = imread('references\waldo2.jpg');
 [ih, iw, ~] = size(image);
+[rh, rw, ~] = size(ref);
 
 imageHsv = rgb2hsv(image);
 h = imageHsv(:, :, 1);
@@ -25,8 +26,10 @@ roi = redDilated & whiteDilated;
 figure,imshow(roi), title("Segmentação por tiras vermelhas e brancas.");
 
 % Remove smaller or bigger components that are not Wally
-roi = bwareaopen(roi, floor(ih/150 * iw/150));
-bigger = bwareaopen(roi, floor(ih/100 * iw/100));
+roi = bwareaopen(roi, floor((ih * iw)/185^2));
+roi = imdilate(roi, se);
+roi = bwareaopen(roi, 20);
+bigger = bwareaopen(roi, floor((ih * iw)/23^2));
 roi = roi - bigger;
 figure,imshow(roi), title("Remoção de elementos menores que o Wally.");
 
@@ -36,22 +39,37 @@ roi = imdilate(roi, se);
 roi = bwareaopen(roi, 10);
 figure,imshow(roi), title("Dilatação e abertura de elementos restantes.");
 
-% Find Wally throught normalized cross correlation
+% Find Wally through normalized cross correlation
 roi = logical(roi);
 c = regionprops(roi, 'Centroid');
 centroids = cat(1, c.Centroid);
+
+locus_x = round(centroids(:,2));
+locus_y = round(centroids(:,1));
+
+x = size(centroids);
+sizeref = size(ref);
+ref = rgb2gray(ref);
+
+for i = 1:x(1)
+    slice = image(max(1, locus_x(i)-100):min(iw, locus_x(i)+100), max(1, locus_y(i)-100):min(ih, locus_y(i)+100));
+    sizeslice = size(slice);
+    if sizeslice(1) >= sizeref(1) && sizeslice(2) >= sizeref(2)
+        C = normxcorr2(ref,slice);
+        tmp = max(C(:));
+        if ~exist('cmax','var')
+            cmax = tmp;
+            coords = [locus_x(i), locus_y(i)];
+        elseif cmax < tmp
+            cmax = tmp;
+            coords = [locus_x(i), locus_y(i)];
+        end
+    end
+end
 figure,imshow(image)
 hold on
 plot(centroids(:,1),centroids(:,2),'b*');
+rectangle('Position', [coords(2)-50, coords(1)-50, 100, 100], 'LineWidth', 3);
 hold off
 
-locus_x = round(centroids(6,2));
-locus_y = round(centroids(6,1));
-
-slice = image(locus_x-100:locus_x+100, locus_y-100:locus_y+100);
-ref = rgb2gray(ref);
-H = vision.TemplateMatcher;
-
-LOC = step(H, slice, ref);
-
-C = normxcorr2(ref,slice);
+figure,imshow(image(max(1,coords(1)-100):min(iw, coords(1)+100), max(1,coords(2)-100):min(ih, coords(2)+100)));
