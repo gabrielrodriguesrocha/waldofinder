cf0b50b531113f9c22b348704f172f56d3bf2167
gabrielrodriguesrocha <gabrielrodrigues_rocha@hotmail.com>
2017-12-11 00:53:46 -0200

    Actual final touches

--- README.md
+++ README.md
@@ -2,8 +2,8 @@
 
 *An answer to the question:  Where's Waldo?*
 
-Este projeto foi desenvolvido para a disciplina Processamento de Imagens e Vis√o Computacional,
-ministrado na Universidade Federal de S√o Carlos (Campus Sorocaba), pelo Prof. Dr. Siovani Cintra Felipussi.
+Este projeto foi desenvolvido para a disciplina Processamento de Imagens e Vis„o Computacional,
+ministrado na Universidade Federal de S„o Carlos (Campus Sorocaba), pelo Prof. Dr. Siovani Cintra Felipussi.
 
-Os arquivos **find&#95;waldo.m** e **find&#95;waldo.mlx** s√o os *scripts* respons√veis pela implementa√√o do projeto.
-As vari√veis **scenario** e **reference** podem ser modificadas conforme desejado.
+Os arquivos **find&#95;waldo.m** e **find&#95;waldo.mlx** s„o os *scripts* respons·veis pela implementaÁ„o do projeto.
+As vari·veis **scenario** e **reference** podem ser modificadas conforme desejado.
--- find_waldo.m
+++ find_waldo.m
@@ -17,7 +17,7 @@ clc, clear, close all;
 
 %% Abertura das imagens
 tic
-scenario = 'scenarios/wheresWaldo4.jpg';
+scenario = 'scenarios/wheresWaldo1.jpg';
 reference = dir('references/*.jpg');
 
 % Imagem cenário
@@ -28,28 +28,29 @@ image = imread(scenario);
 files = {reference.name};
 ref = cell(1,numel(files));
 for k=1:numel(files)
-    ref{k} = imread(strcat('references/',files{k}));
-    ref{k} = rgb2gray(ref{k});
+    ref{k} = rgb2gray(imread(strcat('references/',files{k})));
 end
 
 % Obtenção do tamanho do Waldo
-waldo_size = 0;
-for k = 1:numel(ref)
-    waldo_size = waldo_size + mean(size(ref{k}))/numel(ref);
-end
+ref_size = cellfun(@(x) (mean(size(x))/numel(ref)), ref);
+waldo_size = sum(ref_size);
 
 % Conversão da imagem cenário de RGB para HSV
 imageHsv = rgb2hsv(image);
 h = imageHsv(:, :, 1);
 s = imageHsv(:, :, 2);
-v = imageHsv(:, :, 3);
+v = histeq(imageHsv(:, :, 3));
 
-%%
-% Segmentação por meio de cor
-redStripes = ((h < 0.01) | (h > 0.9)) & (s > 0.7) & (v > 0.9);
+%% Segmentação por meio de cor
+
+if (median(s(:)) < 0.25) % Caso em que a imagem não tem saturação muito boa
+    redStripes = ((h < 0.01) | (h > 0.9)) & (s > 0.7) & (v > median(v(:)) - var(v(:)));
+else
+    redStripes = ((h < 0.01) | (h > 0.9)) & (s > 0.7) & (v > median(v(:)));
+end
 whiteStripes = (s < 0.2) & (v > 0.9);
 
-%%
+%% 
 % Criação de elemento estruturante: linha vertical
 se = strel('line', waldo_size * 0.15, 90);
 
@@ -63,27 +64,27 @@ whiteDilated = imdilate(whiteStripes, se);
 roi = redDilated & whiteDilated;
 figure,imshow(roi), title('Segmentação por tiras vermelhas e brancas.');
 
-%%
-% Remoção de elementos menores ou maiores que não são o Waldo
-rg = regionprops(roi, 'Centroid');
-if numel(rg) <= 10 % Tratamento de caso em que o Waldo é muito pequeno
+%% Remoção de elementos menores ou maiores que não são o Waldo
+rg = regionprops(roi, 'Centroid', 'Area');
+if numel(rg) <= 10 % Heurística para caso em que o Waldo é muito pequeno
     roi = imdilate(roi, strel('square', round(waldo_size * 0.1)));
 end
 roi = bwareaopen(roi, floor(waldo_size * 0.9));
 roi = imdilate(roi, se);
-roi = bwareaopen(roi, 20);
-bigger = bwareaopen(roi, floor(25 * waldo_size));
+bigger = bwareaopen(roi, floor(50 * waldo_size));
+smaller = bwareaopen(roi, floor(0.1 * waldo_size));
 roi = roi - bigger;
-figure,imshow(roi), title('Remoção de elementos menores que o Waldo.');
+figure,imshow(roi), title('Remoção de elementos maiores que o Waldo.');
 
-%%
-% Remoção de ruído dos objetos resultantes
+%% Remoção de ruído dos objetos resultantes
 roi = roi & (redStripes | whiteStripes);
 roi = imdilate(roi, strel('square', round(waldo_size * 0.1)));
-roi = bwareaopen(roi, 12);
 figure,imshow(roi), title('Dilatação e abertura de elementos restantes.');
 
-%%
+%% Busca por Waldo através de iteração e correlação cruzada normalizada
+color_image = image;
+image = rgb2gray(image);
+
 % Obtenção dos centroides da imagem
 roi = logical(roi);
 c = regionprops(roi, 'Centroid');
@@ -93,11 +94,6 @@ locus_y = round(centroids(:,1));
 
 x = size(centroids);
 sizeref = size(ref);
-
-%%
-% Busca por Waldo através de iteração e correlação cruzada normalizada
-color_image = image;
-image = rgb2gray(image);
 
 for i = 1:x(1)
     % Obtenção de um pedaço de interesse
@@ -124,14 +120,14 @@ for i = 1:x(1)
     
 end
 
-%%
+%% Resultado
 % Todos os centroides e a região em que Waldo se encontra
 figure,imshow(color_image),title('Centroides da imagem e Waldo.');
 hold on
 plot(centroids(:,1),centroids(:,2),'b*');
 rectangle('Position', [coords(2)-50, coords(1)-50, 100, 100], 'LineWidth', 3);
 hold off
-%%
+%% Resultado em foco
 % Imagem ampliada de Waldo na imagem cenário
 waldo = color_image(max(1,coords(1)-100):min(ih, coords(1)+100), max(1,coords(2)-100):min(iw, coords(2)+100), :);
 figure,imshow(waldo), title('Waldo!');
